bad6eedbfb914715337d7201acd63f1f7c0a1638
Haoran Zhou <61609879+NoneJou072@users.noreply.github.com>
2024-04-18 14:52:03 +0800

    Update demo_env.py

--- gpt_server/gpt_server/demo_env.py
+++ gpt_server/gpt_server/demo_env.py
@@ -28,7 +28,7 @@ class GraspingEnv(PosCtrlEnv):
             is_pd=is_pd
         )
 
-        self.init_pos, self.init_rot = self.kd_solver.fk(self.robot.arm_qpos, rot_format='quaternion')
+        self.init_pos, self.init_rot = self.kd_solver.fk(self.robot.get_arm_qpos(), rot_format='quaternion')
         self.action = self.init_pos
 
     @primitive
@@ -86,7 +86,7 @@ class GraspingEnv(PosCtrlEnv):
 
     @primitive
     def get_current_pose(self):
-        return self.kd_solver.fk(self.robot.arm_qpos, rot_format='quaternion')
+        return self.kd_solver.fk(self.robot.get_arm_qpos(), rot_format='quaternion')
 
 
 def make_env():
